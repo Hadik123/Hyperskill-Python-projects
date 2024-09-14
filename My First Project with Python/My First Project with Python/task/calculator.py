7504ab671a069511b82d35cf2e6f05c338ad46cb
Hadik123 <lada.hadacek@seznam.cz>
2024-09-14 17:56:46 +0300

    Completed projects
    Adding already completed projects for portfolio

---
+++ My First Project with Python/My First Project with Python/task/calculator.py
@@ -0,0 +1,9 @@
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+count_to = int(input())
+number = 0
+
+while number < count_to:
+    print(number)
+    number = number + 1
